c7d03ad1297331ae06deaed0bb885564240d1890
ABHISHEK KANOUJIA <60141858+abhi1491702@users.noreply.github.com>
2022-12-03 14:41:29 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,153 @@
+  import React from 'react';
+  import ReactDOM from 'react-dom';
+  // import App from './App';
+  // import "./index.css"
+  import App1 from './App1';
+  import "./App1.css"
+
+//   import {sum,sub,div,mul} from './App';
+//  import Cards from './Cards';
+//  import Sdata from './Sdata';
+
+
+// 1 to 17 video ======>>>>>
+
+//   const fname ="Abhishek";
+//   const lname ="Kanoujia";
+//   const date=new Date().toLocaleDateString();
+//   const time=new  Date().toLocaleTimeString();
+//   const imag1="https://picsum.photos/200/100";
+//   const imag2="https://picsum.photos/250/150";
+//   const imag3="https://picsum.photos/220/110";
+//   const imag4="https://picsum.photos/250/130";
+//   const link="https://abhi1491702.github.io/sudoku-solver/"
+
+
+//  const heading={
+//   color: '#fa9191',
+//     textAlign: 'center',
+//     textTransform: 'capitalize',
+//     fontWeight: 'bold',
+//     textShadow: '0px 2px 4px #ffe9c5',
+//     margin: '50px 0px',
+//     fontFamily: '"Josefin Slab", serif',
+//  }
+
+
+
+//  <React.Fragment>
+  {/* <h1 style={heading}>my name is {fname+" "+lname}</h1> 
+
+  {/* <h1> {`my first names is ${fname} and my last name is ${lname}`}</h1>  */}
+  {/* <p><h2>my lucky number is {3*3}</h2></p>   */}
+
+  {/* <div className="img_div">
+   <h1>Todays Date is {date}</h1> 
+   </div>
+   <div className="img_div">
+   <h1>Current time is {time}</h1> 
+  </div> */}
+  
+  {/* <div className="img_div">
+  <img src={imag1} alt="randomImages" />
+  <img src={imag2} alt="randomImages" />
+  <img src={imag3} alt="randomImages" />
+  <a href={link}>
+  <img src={imag4} alt="randomImages" />
+  </a>
+
+  </div> */}
+
+  // </React.Fragment>  
+
+
+
+
+// video  18 =========>>>>>>>>
+
+// let curDate=new Date(); 
+// let greeting='';
+// curDate=curDate.getHours();
+// const cssStyle={ };
+
+
+// if(curDate>=1 && curDate<12)
+// {
+//   greeting='Good Morning';
+//   cssStyle.color='green'; 
+// }
+// else if(curDate>=12 && curDate<19)
+// {
+//   greeting='Good Afternoon';
+//   cssStyle.color='orange'; 
+// }
+// else
+// {
+//   greeting='Good Night';
+//   cssStyle.color='Black'; 
+// }
+
+//<>
+// <div>
+//  <h1>Hello sir, <span style={cssStyle}>{greeting} </span></h1>
+// </div>
+// </>
+
+// video 21 ------->>> 
+{/* <ol>
+        <li>Abhishek </li>
+        <li>{you.default}</li>
+        <li>{you.favprog} </li>
+        <li>{ you.myName()}</li>
+        <li>{ you.myNames()}</li>
+</ol> */}
+
+/// video ----> 22
+
+{/* <li>sum of two no is {sum(40,4)}</li>
+       <li>sub of two no is {sub(30,3)}</li>
+       <li>Div of two no is {div(10,3)}</li>
+       <li>Mult of two no is {mul(10,3)}</li> */}
+
+  // function ncard(val) {
+  //    return (
+  //     <Cards   
+  //       imgsrc={val.imgsrc}
+  //       title={val.title}
+  //       sname={val.sname}
+  //       link={val.link}
+  //     />
+  //    );
+  // }
+
+        
+  ReactDOM.render(<App1/>,document.getElementById("root")
+
+  );
+
+  // const fullname=["vinod","thapa"];
+  // const biodata=[1,...fullname,26,"male"];
+
+  // console.log(fullname);
+  // console.log(biodata);
+
+
+  // var shooterGame=["call of Duty","Far Cry","Resident Evil"];
+  // var [first,...remaining]=shooterGame;
+  // console.log(first);
+  // console.log(remaining);
+
+  // const fullName={
+  //   fname: "Abhishek",
+  //   lname:  "Kanoujia",
+  // }
+
+  // const biodata={
+  //   id:1,
+  //   ...fullName,
+  //   age:26,
+  //   gender:"male",  
+
+  // }
+
+  // console.log(biodata);
